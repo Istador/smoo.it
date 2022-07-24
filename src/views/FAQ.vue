6a628138ce308aeb3bf28a9682558048b30bc9b7
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-07-24 13:57:25 +0200

    improve faq

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="faq-view accordion">
-    <b-card v-for="(item, i) in questions" :key="item.question" no-body>
-      <b-card-header header-tag="header" v-b-toggle="'accordion-' + i">
-        {{ item.question }}
-      </b-card-header>
+    <b-card
+      v-for="(item, i) in questions"
+      :key="item.question"
+      no-body
+    >
+      <b-card-header
+        header-tag="header"
+        v-b-toggle="'accordion-' + i"
+        v-html="item.question"
+      />
       <b-collapse :id="'accordion-' + i" visible accordion="questions">
         <b-card-body><component :is="{ template: '<div>' + item.answer + '</div>' }"/></b-card-body>
       </b-collapse>
@@ -52,14 +58,12 @@ export default class FAQ extends Vue {
             <li><smoo-btn ZL/> + <smoo-btn A/> - Mod configuration</li>
           </ul>
         </p>
-
         <p>
           Pause menu:
           <ul>
             <li><smoo-btn ZL/> + <smoo-btn A/> or <smoo-btn ZL/> + <smoo-btn B/> - Mod configuration</li>
           </ul>
         </p>
-
         <p>
           In-game:
           <ul>
@@ -82,7 +86,10 @@ export default class FAQ extends Vue {
       answer   : `
         By one of these methods:
         <ul>
-          <li>In the main or pause menu hold <smoo-btn ZL/> and press <smoo-btn A/> to enter the hidden options menu where you can <code>Change Server IP</code>.</li>
+          <li>
+            In the main or pause menu hold <smoo-btn ZL/> and press <smoo-btn A/> to enter the hidden options menu
+            where you can <code>Change Server IP</code>.
+          </li>
           <li>Hold/press <smoo-btn ZL/> while starting the game, will prompt you for the IP.</li>
           <li>Delete the <code>common.bin</code> file in the save directory.</li>
         </ul>
@@ -92,28 +99,41 @@ export default class FAQ extends Vue {
       question : 'How do I hack my Switch?',
       answer   : `
         <p>
-          You need an <a href="https://ismyswitchpatched.com/" class="extern" target="_blank">unpatched</a> older revision of the Nintendo Switch,
-          a microSDXC card, and a USB-C connection cable.
+          You need an <a href="https://ismyswitchpatched.com/" class="extern" target="_blank">unpatched</a>
+          older revision of the Nintendo Switch, a microSDXC card, a USB-C connection cable,
+          and a computer (PC/Laptop/Smartphone) or a specific paylod injecting device.
           To make the RCM exploit safer and easier, it's advised to use a RCM jig.
         </p>
-
         <p>
           Follow this <a href="https://nh-server.github.io/switch-guide/" class="extern" target="_blank">guide</a>
-          or alternatively <a href="https://switch.homebrew.guide/" class="extern" target="_blank">this one</a>.
+          or alternatively/additionally <a href="https://switch.homebrew.guide/" class="extern" target="_blank">this one</a>.
         </p>
       `,
     },
     {
       question : 'How do I obtain a SMO ROM for emulators?',
-      answer   : 'The only <i>legal</i> way to obtain a ROM is by dumping it from your hacked Switch.',
+      answer   : `
+        <p>The only <i>legal</i> way to obtain a ROM is by dumping it from your hacked Switch onto the SD card.</p>
+      `,
     },
     {
-      question : 'Am I on SMO version 1.0.0?',
-      answer   : 'If the game shows a <code>Play in VR</code> button, then you are still on version 1.3.',
+      question : 'Am I on SMO version <code>1.0.0</code>?',
+      answer   : `
+        <p>
+          When the game is downgraded on the Switch, the system and the main menu will still show <code>1.3.0</code>, which is correct.
+        </p>
+        <p>
+          A good indicator if the game is on version <code>1.3.0</code> or lower is the <code>Play in VR</code> button in the main menu.
+          When the button is visible that means the downgrade didn't work and the game is still on version <code>1.3.0</code>.
+        </p>
+        <p>
+          For emulators a real <code>1.0.0</code> ROM is needed, which can be dumped from the Switch.
+        </p>
+      `,
     },
     {
-      question : 'Will the mod be available for SMO version 1.3 in the future?',
-      answer   : 'No, it\'s too much effort to develop and support different versions of the mod.',
+      question : 'Will the mod work for SMO version <code>1.3.0</code> in the future?',
+      answer   : 'No, it\'s too much effort to develop, test, build, release and support different versions of the mod.',
     },
     {
       question : 'What is a public/private IPv4 address?',
@@ -123,19 +143,16 @@ export default class FAQ extends Vue {
           Usually your router receives a public IPv4 address from your ISP.
           But be aware that there are internet subscriptions that don't give you a public IPv4 address but only a IPv6 address (e.g. LTE, DS Lite).
         </p>
-
         <p>
           Private IPv4 addresses are usually only used inside your home network or for VPN networks and aren't publicly reachable from the internet.
           You can easily detect them by their leading numbers:
           <code>10.x.x.x</code>, <code>172.[16-31].x.x</code>, and <code>192.168.x.x</code> are all private addresses.
         </p>
-
         <p>
           Additionally <code>127.x.x.x</code> is reserved for the local computer.
           You can input <code>127.0.0.1</code> as the server IP in the <router-link :to="{ name: 'play' }">mod</router-link>,
           if you're playing with an emulator on the same computer that runs the <router-link :to="{ name: 'host' }">server</router-link>.
         </p>
-
         <p>
           <code>0.0.0.0</code> usually stands for an invalid IPv4 address.
           It is only valid in context of hosting a <router-link :to="{ name: 'host' }">server</router-link>
@@ -147,16 +164,17 @@ export default class FAQ extends Vue {
     {
       question : 'Does this mod work with IPv6?',
       answer   : `
-        No.
-        The Nintendo Switch doesn't support IPv6 natively.
-        As long as Nintendo doesn't change this, or there's a homebrew network stack that support it, this'll likely never change.
+        <p>
+          No.
+          The Nintendo Switch doesn't support IPv6 natively.
+          As long as Nintendo doesn't change this, or there's a homebrew network stack that support it, this'll likely never change.
+        </p>
       `,
     },
     {
       question : 'How do I port forward?',
       answer   : `
-        <p>This depends on your concrete router model.</p>
-
+        <p>This depends on your specific router model.</p>
         <p>
           Usually you open the web interface of your router, login there, and search for an option that let you configure port forwarding.
           Common router addresses are:
@@ -167,7 +185,6 @@ export default class FAQ extends Vue {
           If you can't figure it out, check the outside of your router (usually there's a sticker on it),
           consult its manual, or check the default gateway of your network adapter.
         </p>
-
         <p>
           Once you found the settings, you want to configure:
           <ul>
@@ -176,8 +193,9 @@ export default class FAQ extends Vue {
           </ul>
           Additionally you also need to say to which computer inside your network the port shall be forwarded to.
           You want to forward the port to the computer that runs the <router-link :to="{ name: 'host' }">server</router-link>.
-          Most routers will give you the option to select one of the currently connected devices,
-          but sometimes you need to provide its private IP address.
+          Most routers will give you the option to select one of the currently connected devices.
+          (Or you have already selected it to reach the port forwarding settings.)
+          But sometimes you need to provide its private IP address manually.
         </p>
       `,
     },
