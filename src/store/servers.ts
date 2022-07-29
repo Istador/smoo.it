88dc39e97568ed012787a423f3f79e85c9ddd23e
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-07-29 02:57:01 +0200

    Servers: move config into own file

---
+++ src/store/servers.ts
@@ -0,0 +1,57 @@
+import { IServer } from '@/types'
+
+const github = 'https://github.com/Sanae6/SmoOnlineServer/'
+const externLink = (href: string, text: string) => '<a href="' + href + '" class="extern" target="_blank">' + text + '</a>'
+const linkRelease = (tag: string, label?: string) => externLink(github + 'releases/tag/' + tag, (label || tag))
+const linkTree = (branch: string, label?: string) => externLink(github + 'commits/' + branch, (label || branch))
+
+export const servers : IServer[] = [
+  {
+    name     : 'Piplup',
+    server   : { host: 'piplup.smoo.it', ip: '51.178.136.142' },
+    location : { flag: 'fr', name: 'France' },
+  },
+  {
+    name     : 'Piplup (Temp)',
+    server   : { host: 'piplup.smoo.it', ip: '51.178.136.142', port: 1806 },
+    location : { flag: 'fr', name: 'France' },
+    version  : linkTree('packet-fixes'),
+  },
+  {
+    name     : 'Sanae',
+    server   : { host: 'sanae.smoo.it', ip: '64.201.219.20' },
+    location : { flag: 'ca', name: 'Canada' },
+  },
+  {
+    name     : 'Parknich',
+    server   : { host: 'parknich.smoo.it', ip: '51.81.86.202' },
+    location : { flag: 'us', name: 'US-East' },
+  },
+  {
+    name     : 'fruityloops',
+    server   : { host: 'fruityloops.smoo.it', ip: '62.226.142.89' },
+    location : { flag: 'de', name: 'Germany' },
+  },
+  {
+    name     : 'f0c0s',
+    server   : { host: 'f0c0s.smoo.it', ip: '94.130.25.137' },
+    location : { flag: 'de', name: 'Germany' },
+  },
+  {
+    name     : 'beni',
+    server   : { host: 'beni.smoo.it', ip: '51.68.173.172' },
+    location : { flag: 'de', name: 'Germany' },
+  },
+  {
+    name     : 'RCL',
+    server   : { host: 'rcl.smoo.it' },
+    location : { flag: 'de', name: 'Germany' },
+    version  : linkRelease('1.0.2', '1.1.0'),
+  },
+  {
+    name     : 'RCL',
+    server   : { host: 'rcl.smoo.it', port: 1028 },
+    location : { flag: 'de', name: 'Germany' },
+    version  : linkTree('packet-fixes'),
+  },
+].map((s: IServer) => { s.server.state = null; return s })
--- src/views/Servers.ts
+++ src/views/Servers.ts
@@ -5,10 +5,7 @@ import CountryFlag from 'vue-country-flag'
 import { IServer } from '@/types'
 import SmooServerState from '@/components/SmooServerState.vue'
 
-const github = 'https://github.com/Sanae6/SmoOnlineServer/'
-const externLink = (href: string, text: string) => '<a href="' + href + '" class="extern" target="_blank">' + text + '</a>'
-const linkRelease = (tag: string, label?: string) => externLink(github + 'releases/tag/' + tag, (label || tag))
-const linkTree = (branch: string, label?: string) => externLink(github + 'commits/' + branch, (label || branch))
+import { servers } from '@/store/servers'
 
 @Component({
   components: {
@@ -17,9 +14,9 @@ const linkTree = (branch: string, label?: string) => externLink(github + 'commit
   },
 })
 export default class Servers extends Vue {
-  private defaultPort = 1027
-
-  private fields = [
+  defaultPort = 1027
+  servers = servers
+  fields = [
     {
       key     : 'state',
       tdClass : 'td-state',
@@ -59,57 +56,6 @@ export default class Servers extends Vue {
       thClass       : 'th-version',
       sortable      : true,
       sortDirection : 'desc',
-    },
-  ]
-
-  private servers : IServer[] = [
-    {
-      name     : 'Piplup',
-      server   : { host: 'piplup.smoo.it', ip: '51.178.136.142' },
-      location : { flag: 'fr', name: 'France' },
-    },
-    {
-      name     : 'Piplup (Temp)',
-      server   : { host: 'piplup.smoo.it', ip: '51.178.136.142', port: 1806 },
-      location : { flag: 'fr', name: 'France' },
-      version  : linkTree('packet-fixes'),
-    },
-    {
-      name     : 'Sanae',
-      server   : { host: 'sanae.smoo.it', ip: '64.201.219.20' },
-      location : { flag: 'ca', name: 'Canada' },
-    },
-    {
-      name     : 'Parknich',
-      server   : { host: 'parknich.smoo.it', ip: '51.81.86.202' },
-      location : { flag: 'us', name: 'US-East' },
-    },
-    {
-      name     : 'fruityloops',
-      server   : { host: 'fruityloops.smoo.it', ip: '62.226.142.89' },
-      location : { flag: 'de', name: 'Germany' },
-    },
-    {
-      name     : 'f0c0s',
-      server   : { host: 'f0c0s.smoo.it', ip: '94.130.25.137' },
-      location : { flag: 'de', name: 'Germany' },
-    },
-    {
-      name     : 'beni',
-      server   : { host: 'beni.smoo.it', ip: '51.68.173.172' },
-      location : { flag: 'de', name: 'Germany' },
-    },
-    {
-      name     : 'RCL',
-      server   : { host: 'rcl.smoo.it' },
-      location : { flag: 'de', name: 'Germany' },
-      version  : linkRelease('1.0.2', '1.1.0'),
-    },
-    {
-      name     : 'RCL',
-      server   : { host: 'rcl.smoo.it', port: 1028 },
-      location : { flag: 'de', name: 'Germany' },
-      version  : linkTree('packet-fixes'),
     },
   ]
 }
