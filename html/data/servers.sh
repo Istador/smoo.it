1c9e3b621359dffeb95f9a775add11b767924573
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-08-24 23:51:50 +0200

    remove jonah.smoo.it

--- html/data/servers.sh
+++ html/data/servers.sh
@@ -23,7 +23,6 @@ scan_all() {
     rcl.smoo.it:1028
     krokilex.smoo.it
     joe.smoo.it
-    jonah.smoo.it
   )
   local stamp=`date --iso-8601=seconds`
   local output=(`parallel --colsep=:  -j 4  -k  --nice 19  scan_one  :::  "${servers[@]}"`)
