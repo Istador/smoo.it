95b319165f80dba465a3934d23949f8895402200
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-09-04 19:50:06 +0200

    fix: loading state for servers with live data

--- src/components/SmooServerState.vue
+++ src/components/SmooServerState.vue
@@ -4,13 +4,13 @@
     :class="{ 'state-component': true, [ state ]: true }"
   >
     <b-icon
-      v-if="!result || typeof result !== 'object' || !players"
+      v-if="!result || typeof result !== 'object' || !players || state != 'online'"
       :icon="icon"
       :animation="animation"
     />
 
     <b-btn
-      v-if="result && typeof result === 'object' && players"
+      v-if="result && typeof result === 'object' && players && state == 'online'"
       size="sm"
       :variant="(tooOld ? 'outline-success' : 'success')"
       v-b-modal="'smoo-server-state-players-' + _uid"
@@ -30,25 +30,27 @@
         Players on <code>{{ name }}</code>
       </template>
 
-      <table class="table table-striped table-borderless table-sm mb-0 smoo-server-players">
-        <thead>
-          <tr>
-            <th>Player</th>
-            <th>Stage</th>
-            <th>Costume</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(player, idx) of players" :key="name + ';' + idx + ';' + player.Name">
-            <td>{{ player.Name || '?' }}</td>
-            <td>{{ stages[player.Stage] || player.Stage || '' }}</td>
-            <td class="costume">
-              <div class="costume-cap"  v-if="player.Costume && player.Costume.Cap">{{ player.Costume.Cap }}</div>
-              <div class="costume-body" v-if="player.Costume && player.Costume.Body">{{ player.Costume.Body }}</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <b-overlay :show="state == 'loading'">
+        <table class="table table-striped table-borderless table-sm mb-0 smoo-server-players">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Stage</th>
+              <th>Costume</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(player, idx) of players" :key="name + ';' + idx + ';' + player.Name">
+              <td>{{ player.Name || '?' }}</td>
+              <td>{{ stages[player.Stage] || player.Stage || '' }}</td>
+              <td class="costume">
+                <div class="costume-cap"  v-if="player.Costume && player.Costume.Cap">{{ player.Costume.Cap }}</div>
+                <div class="costume-body" v-if="player.Costume && player.Costume.Body">{{ player.Costume.Body }}</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </b-overlay>
     </b-modal>
   </span>
 </template>
