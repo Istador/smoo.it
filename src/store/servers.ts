d019b056512cb41f38004eb3f2116929fe2af8ca
Robin C. Ladiges <rcl.git@blackpinguin.de>
2022-10-12 01:00:44 +0200

    servers: RCL 1 version bump

--- src/store/servers.ts
+++ src/store/servers.ts
@@ -47,7 +47,7 @@ export const servers: IServer[] = [
     name     : 'RCL 1',
     server   : { host: 'rcl.smoo.it' },
     location : { flag: 'de', name: 'Germany' },
-    version  : linkRelease('1.0.4-rcl.1', 'rcl/1.0.4', 'Istador/SmoOnlineServer'),
+    version  : linkRelease('1.0.4-rcl.2', 'rcl/1.0.4', 'Istador/SmoOnlineServer'),
     settings : {
       Server        : { MaxPlayers: 6 },
       Scenario      : { MergeEnabled: false },
