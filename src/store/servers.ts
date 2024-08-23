425874233b11a61d61f325392389df9ca16875fe
Luna Broeke <107153704+LunaSakura154@users.noreply.github.com>
2024-08-23 20:34:44 +0200

    Luwuna H&S + Manhunt Servers

--- src/store/servers.ts
+++ src/store/servers.ts
@@ -170,6 +170,41 @@ export const servers: IServer[] = [
       Shines        : { Enabled: true },
       PersistShines : { Enabled: false },
     },
+    {
+    name     : 'Luwuna (H&S)',
+    link     : '5.180.182.219',
+    server   : { ip: '5.180.182.219', port:1027 },
+    location : { flag: 'nl', name: 'Netherlands' },
+    settings : {
+      Server        : { MaxPlayers: 8 },
+      Scenario      : { MergeEnabled: true },
+      Shines        : { Enabled: true },
+      PersistShines : { Enabled: false },
+    },
+    {
+    name     : 'Luwuna (Manhunt 01)',
+    link     : '5.180.182.219',
+    server   : { ip: '5.180.182.219', port:1028 },
+    location : { flag: 'nl', name: 'Netherlands' },
+    version : externLink("https://gamebanana.com/mods/536910", "This server is meant to be used with Secret Dev's Manhunt mod"),
+    settings : {
+      Server        : { MaxPlayers: 4 },
+      Scenario      : { MergeEnabled: false },
+      Shines        : { Enabled: false },
+      PersistShines : { Enabled: false },
+    },
+    {
+    name     : 'Luwuna (Manhunt 02)',
+    link     : '5.180.182.219',
+    server   : { ip: '5.180.182.219', port:1029 },
+    location : { flag: 'nl', name: 'Netherlands' },
+    version : externLink("https://gamebanana.com/mods/536910", "This server is meant to be used with Secret Dev's Manhunt mod"),
+    settings : {
+      Server        : { MaxPlayers: 4 },
+      Scenario      : { MergeEnabled: false },
+      Shines        : { Enabled: false },
+      PersistShines : { Enabled: false },
+    },
   },
 ].map((s: IServer) => {
   s.server.state = null
