e7674644e706cb1bab3541644ace69662cf53985
Robin C. Ladiges <rcl.git@blackpinguin.de>
2024-10-20 12:41:39 +0200

    servers: RCL 1-4 server version

--- src/store/servers.ts
+++ src/store/servers.ts
@@ -136,7 +136,7 @@ export const servers: IServer[] = [
     name     : 'RCL 1 (H&S)',
     server   : { host: 'rcl.smoo.it' },
     location : { flag: 'de', name: 'Germany' },
-    version  : linkRelease('1.0.5-rcl.6', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
+    version  : linkRelease('1.0.5-rcl.7', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
     settings : {
       Server        : { MaxPlayers: 6 },
       Scenario      : { MergeEnabled: true },
@@ -148,7 +148,7 @@ export const servers: IServer[] = [
     name     : 'RCL 2 (100%)',
     server   : { host: 'rcl.smoo.it', port: 1028 },
     location : { flag: 'de', name: 'Germany' },
-    version  : linkRelease('1.0.5-rcl.6', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
+    version  : linkRelease('1.0.5-rcl.7', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
     settings : {
       Server        : { MaxPlayers: 6 },
       Scenario      : { MergeEnabled: true },
@@ -160,7 +160,7 @@ export const servers: IServer[] = [
     name     : 'RCL 3 (Co-Op)',
     server   : { host: 'rcl.smoo.it', port: 1029 },
     location : { flag: 'de', name: 'Germany' },
-    version  : linkRelease('1.0.5-rcl.6', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
+    version  : linkRelease('1.0.5-rcl.7', 'rcl/1.0.5', 'Istador/SmoOnlineServer'),
     settings : {
       Server        : { MaxPlayers: 6 },
       Scenario      : { MergeEnabled: true },
@@ -179,7 +179,7 @@ export const servers: IServer[] = [
       PersistShines : { Enabled: false },
     },
     version: {
-      html    : linkRelease('1.0.5-rcl.2', 'udp/1.0.5', 'Istador/smo-multi-rs'),
+      html    : linkRelease('1.0.5-rcl.3', 'udp/1.0.5', 'Istador/smo-multi-rs'),
       details : `
         <p>Connecting requires mod version ${linkRelease('v1.4.0', 'v1.4.0', 'CraftyBoss/SuperMarioOdysseyOnline')} or later.</p>
       `,
