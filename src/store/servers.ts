1f029c41bc3fade74521e7621b51c7b4c191ce33
Skylanderfree <tzorinjose9@gmail.com>
2026-06-02 10:33:01 -0500

    Update src/store/servers.ts
    Co-authored-by: Robin C. Ladiges <Istador@users.noreply.github.com>

--- src/store/servers.ts
+++ src/store/servers.ts
@@ -106,7 +106,7 @@ export const servers: IServer[] = [
     settings : {
       Server        : { MaxPlayers: 12 },
       Scenario      : { MergeEnabled: true },
-      Shines        : { Enabled: true },
+      Shines        : { Enabled: true, ClearOnNewSaves: true },
       PersistShines : { Enabled: true },
     },
   },
